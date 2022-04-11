c92dc76cc17572b7184e81404148edde9a3945da
Grayson Miller <graysonmiller@Graysons-MacBook-Pro.local>
2022-04-11 13:59:36 -0400

    added mutations for login, addUser, addThought, addReaction, addFriend.

---
+++ server/utils/auth.js
@@ -0,0 +1,41 @@
+const jwt = require('jsonwebtoken');
+
+const secret = 'mysecretsshhhhh';
+const expiration = '2h';
+
+module.exports = {
+  authMiddleware: function({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    // ["Bearer", "<tokenvalue>"]
+    if (req.headers.authorization) {
+      token = token
+        .split(' ')
+        .pop()
+        .trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
+
+    return req;
+  },
+  signToken: function({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  }
+};
+
+// The signToken() function expects a user object and will add that user's username, email, and _id properties to the token.
+// Optionally, tokens can be given an expiration date and a secret to sign the token with. Note that the secret has nothing to do with encoding.
+// The secret merely enables the server to verify whether it recognizes this token.
