610572afed686d78ccf4737255ac2333967cabc7
Grayson Miller <graysonmiller@Graysons-MacBook-Pro.local>
2022-04-12 15:49:17 -0400

    added routes to view single thoughts, profiles, and display friends lists.

--- client/src/components/ThoughtList/index.js
+++ client/src/components/ThoughtList/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 const ThoughtList = ({ thoughts, title }) => {
   if (!thoughts.length) {
@@ -13,15 +15,25 @@ const ThoughtList = ({ thoughts, title }) => {
         thoughts.map(thought => (
           <div key={thought._id} className="card mb-3">
             <p className="card-header">
-              {thought.username}
+              {/* Link to the username associated with the thought (the poster) by clicking on it. */}
+              <Link
+                to={`/profile/${thought.username}`}
+                style={{ fontWeight: 700 }}
+                className="text-light"
+              >
+                {thought.username}
+              </Link>{' '}
               thought on {thought.createdAt}
             </p>
             <div className="card-body">
-              <p>{thought.thoughtText}</p>
-              <p className="mb-0">
-                Reactions: {thought.reactionCount} || Click to{' '}
-                {thought.reactionCount ? 'see' : 'start'} the discussion!
-              </p>
+              {/* Link to the SingleThought by clicking on the card */}
+              <Link to={`/thought/${thought._id}`}>
+                <p>{thought.thoughtText}</p>
+                <p className="mb-0">
+                  Reactions: {thought.reactionCount} || Click to{' '}
+                  {thought.reactionCount ? 'see' : 'start'} the discussion!
+                </p>
+              </Link>
             </div>
           </div>
         ))}
